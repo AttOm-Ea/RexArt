0877763db1eb8a4701e691866da17c5e1d2d605e
Edwin Alberto <leonora.ea@gmail.com>
2022-04-12 14:51:35 -0500

    start proyect

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Carousel
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems,{
+        indicators: true,
+        duration: 300,
+        numVisible: 5,
+    });
+
+
+    // Parallax
+
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+
+    // Btn emergente
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+
+});
